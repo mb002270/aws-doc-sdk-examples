428af7e219c8cb062346fe9f46d8ac76d22733dd
EC2 Default User <ec2-user@ip-172-24-113-251.ec2.internal>
2018-03-08 04:01:08 +0000

    update

--- java/example_code/s3/commands.bash
+++ java/example_code/s3/commands.bash
@@ -1,1 +1,9 @@
-dd if=/dev/urandom of=~/bigdata/bigfile4.txt bs=10485760 count=100
+#!/bin/bash
+
+sudo mkdir /mnt/ramdisk
+sudo mount -t tmpfs -o size=8192m tmpfs /mnt/ramdisk
+dd if=/dev/urandom of=/mnt/ramdisk/bigfile1.txt bs=10485760 count=100
+dd if=/dev/urandom of=/mnt/ramdisk/bigfile2.txt bs=10485760 count=100
+dd if=/dev/urandom of=/mnt/ramdisk/bigfile3.txt bs=10485760 count=100
+dd if=/dev/urandom of=/mnt/ramdisk/bigfile4.txt bs=10485760 count=100
+dd if=/dev/urandom of=/mnt/ramdisk/bigfile5.txt bs=10485760 count=100
