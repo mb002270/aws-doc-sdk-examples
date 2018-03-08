4c57d0394dde77859be5361e1ef093afad983a6e
EC2 Default User <ec2-user@ip-172-24-113-107.ec2.internal>
2018-03-08 04:35:41 +0000

    update

--- java/example_code/s3/commands.bash
+++ java/example_code/s3/commands.bash
@@ -7,3 +7,6 @@ dd if=/dev/urandom of=/mnt/ramdisk/bigfile2.txt bs=10485760 count=100
 dd if=/dev/urandom of=/mnt/ramdisk/bigfile3.txt bs=10485760 count=100
 dd if=/dev/urandom of=/mnt/ramdisk/bigfile4.txt bs=10485760 count=100
 dd if=/dev/urandom of=/mnt/ramdisk/bigfile5.txt bs=10485760 count=100
+
+
+./run_example.sh XferMgrUpload "mwaters-s3fuse-test /mnt/ramdisk"
