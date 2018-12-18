df5d97998ae66b7a3d0f473a9c0db886e235904c
EC2 Default User <ec2-user@ip-172-30-0-73.ec2.internal>
2018-12-18 03:38:30 +0000

    sending a few messages at once

--- java/example_code/sqs/src/main/java/aws/example/sqs/SendReceiveMessages.java
+++ java/example_code/sqs/src/main/java/aws/example/sqs/SendReceiveMessages.java
@@ -1,3 +1,11 @@
+//snippet-sourcedescription:[<<FILENAME>> demonstrates how to send, receive and delete messages from a queue.]
+//snippet-keyword:[Java]
+//snippet-keyword:[Code Sample]
+//snippet-keyword:[Amazon Simple Queue Service]
+//snippet-service:[sqs]
+//snippet-sourcetype:[full-example]
+//snippet-sourcedate:[]
+//snippet-sourceauthor:[soo-aws]
 /*
  * Copyright 2011-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
@@ -5,7 +13,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at:
  *
- *    http://aws.amazon.com/apache2.0
+ *	http://aws.amazon.com/apache2.0
  *
  * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
  * OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -26,47 +34,102 @@ import java.util.List;
 
 public class SendReceiveMessages
 {
-    private static final String QUEUE_NAME = "testQueue" + new Date().getTime();
+	private static final String QUEUE_NAME = "mwaters-test-queue";
 
-    public static void main(String[] args)
-    {
-        final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+	public static void main(String[] args)
+	{
+		final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
 
-        try {
-            CreateQueueResult create_result = sqs.createQueue(QUEUE_NAME);
-        } catch (AmazonSQSException e) {
-            if (!e.getErrorCode().equals("QueueAlreadyExists")) {
-                throw e;
-            }
-        }
+		try {
+			CreateQueueResult create_result = sqs.createQueue(QUEUE_NAME);
+		} catch (AmazonSQSException e) {
+			if (!e.getErrorCode().equals("QueueAlreadyExists")) {
+				throw e;
+			}
+		}
 
-        String queueUrl = sqs.getQueueUrl(QUEUE_NAME).getQueueUrl();
+		String queueUrl = sqs.getQueueUrl(QUEUE_NAME).getQueueUrl();
 
-        SendMessageRequest send_msg_request = new SendMessageRequest()
-                .withQueueUrl(queueUrl)
-                .withMessageBody("hello world")
-                .withDelaySeconds(5);
-        sqs.sendMessage(send_msg_request);
+		int i=0;
+		long start,end;
+		start = System.currentTimeMillis();
+		for(i=0;i<100;i++)
+		{
+			sendDummyMessage(sqs,queueUrl,"dummy");
+		}
+		end = System.currentTimeMillis();
 
+		System.out.print("Took " + (end-start) + " milliseconds to send " + i + " messages\n");
 
-        // Send multiple messages to the queue
-        SendMessageBatchRequest send_batch_request = new SendMessageBatchRequest()
-                .withQueueUrl(queueUrl)
-                .withEntries(
-                        new SendMessageBatchRequestEntry(
-                                "msg_1", "Hello from message 1"),
-                        new SendMessageBatchRequestEntry(
-                                "msg_2", "Hello from message 2")
-                                .withDelaySeconds(10));
-        sqs.sendMessageBatch(send_batch_request);
+		start = System.currentTimeMillis();
+		long total = receiveAllMessages(sqs, queueUrl);
+		end = System.currentTimeMillis();
+		System.out.print("Took " + (end-start) + " milliseconds to receive " + total + " messages\n");
 
-        // receive messages from the queue
-        List<Message> messages = sqs.receiveMessage(queueUrl).getMessages();
+//		for(int i=0;i<10;i++)
+//		{
+//			String time = Long.toString(System.currentTimeMillis());
+//			SendMessageRequest send_msg_request = new SendMessageRequest()
+//					.withQueueUrl(queueUrl)
+//					.withMessageBody(time);
+//			sqs.sendMessage(send_msg_request);
+//
+//
+//			// Send multiple messages to the queue
+//			SendMessageBatchRequest send_batch_request = new SendMessageBatchRequest()
+//					.withQueueUrl(queueUrl)
+//					.withEntries(
+//							new SendMessageBatchRequestEntry(
+//									"msg_1", "Hello from message 1"),
+//							new SendMessageBatchRequestEntry(
+//									"msg_2", "Hello from message 2")
+//									.withDelaySeconds(10));
+//			sqs.sendMessageBatch(send_batch_request);
+//	
+//			// receive messages from the queue
+//			List<Message> messages = sqs.receiveMessage(queueUrl).getMessages();
+//	
+//			// delete messages from the queue
+//			for (Message m : messages) {
+//				long rec_time = System.currentTimeMillis();
+//				long send_time = Long.parseLong(m.getBody());
+//					long total_time = rec_time - send_time;
+//					long per_second = 1000/total_time;
+//				System.out.println("Took: " + (rec_time-send_time) + " milliseconds ("+ per_second + " per second)");
+//				sqs.deleteMessage(queueUrl, m.getReceiptHandle());
+//			}
+//		}	
+	}
 
-        // delete messages from the queue
-        for (Message m : messages) {
-            sqs.deleteMessage(queueUrl, m.getReceiptHandle());
-        }
-    }
+	private static void sendDummyMessage(final AmazonSQS sqs, String queueUrl,String message)
+	{
+		SendMessageRequest send_msg_request = new SendMessageRequest()
+			.withQueueUrl(queueUrl)
+			.withMessageBody(message);
+		sqs.sendMessage(send_msg_request);
+	}
+
+	private static long receiveAllMessages(final AmazonSQS sqs, String queueUrl)
+	{
+		boolean keep_reading=true;
+		long total = 0;
+		while(keep_reading)
+		{
+			keep_reading=false;
+			List<Message> messages = sqs.receiveMessage(queueUrl).getMessages();
+	
+			for(Message m: messages)
+			{
+				keep_reading=true;
+				total++;
+				String body = m.getBody();
+				if (null == body) 
+				{
+					System.out.println("null body");
+				}
+				sqs.deleteMessage(queueUrl, m.getReceiptHandle());
+			}
+		}
+		return total;
+	}
 }
-
